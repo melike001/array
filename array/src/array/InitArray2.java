01c2529614d56e70596ae805bdaeb5f5e383859e
melik <melik@10.48.108.79>
2022-10-27 10:21:04 +0300

    third

---
+++ array/src/array/InitArray2.java
@@ -0,0 +1,17 @@
+package array;
+
+public class InitArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int[] array = new int[10];
+		
+		System.out.printf("%s%8s%n","Index","Value");
+		
+		for(int counter =0; counter < array.length; counter++ ) {
+			System.out.printf("%5d%8d%n", counter, array[counter]);
+		}
+
+	}
+
+}
